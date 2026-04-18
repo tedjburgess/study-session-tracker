002dd3a6760c327e8f3e488988ff01cc3f1811b5
tedjburgess <ted.j.burgess@gmail.com>
2026-04-18 22:45:05 +0200

    feat: add GET studySession by id route

--- backend/src/routes/studySessionRoutes.js
+++ backend/src/routes/studySessionRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { getStudySessions, createStudySession } = require("../controllers/studySessionController"); //Go to controller file and take out function getStudySessions
+const { getStudySessions, createStudySession, getStudySessionById } = require("../controllers/studySessionController"); //Go to controller file and take out function getStudySessions
 
 router.get("/", getStudySessions);
 router.post("/", createStudySession);
+router.get("/:id", getStudySessionById);
 
 module.exports = router;
