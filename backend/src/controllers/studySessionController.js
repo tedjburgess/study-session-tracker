32839d709faeccea2197dfb53fe170ae56ff4404
tedjburgess <ted.j.burgess@gmail.com>
2026-04-19 10:21:33 +0200

    feat: add PATCH studySession route

--- backend/src/controllers/studySessionController.js
+++ backend/src/controllers/studySessionController.js
@@ -57,9 +57,64 @@ const createStudySession = async (req, res) => {
   }
 };
 
+const patchStudySessionById = async (req, res) => {
+  try {
+    const sessionId = req.params.id;
+    if (!require("mongoose").Types.ObjectId.isValid(sessionId)) {
+      return res.status(400).json({ message: "Invalid study session id format" });
+    }
+
+    const fieldsToUpdate = req.body;
+    const allowedFields = [
+      "courseId",
+      "goalId",
+      "sessionDate",
+      "durationMinutes",
+      "focusRating",
+      "topic",
+      "studyMethod",
+      "notes",
+    ];
+
+    const updateKeys = Object.keys(fieldsToUpdate);
+
+    if (updateKeys.length === 0) {
+      return res.status(400).json({
+        message: "At least one field is required for update",
+      });
+    }
+
+    const hasInvalidField = updateKeys.some(
+      (field) => !allowedFields.includes(field)
+    );
+
+    if (hasInvalidField) {
+      return res.status(400).json({
+        message: "Request body contains invalid update fields",
+      });
+    }
+
+    const updateSession = await StudySession.findByIdAndUpdate(
+      sessionId,
+      fieldsToUpdate,
+      { new: true } //Makes sure to return the updated session
+    );
+
+    if (!updateSession) {
+      return res.status(404).json({
+        message: "No study session associated with the requested id",
+      });
+    }
+
+    res.status(200).json(updateSession);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to update study session" });
+  }
+};
+
 module.exports = {
   getStudySessions,
   createStudySession,
-  getStudySessionById
+  getStudySessionById,
+  patchStudySessionById
 };
-
