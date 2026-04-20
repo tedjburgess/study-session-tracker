edad0b1b96a6e320fb969119d173d84b00132c76
tedjburgess <ted.j.burgess@gmail.com>
2026-04-20 10:31:47 +0200

    feat: create frontend component skeleton

---
+++ frontend/src/components/StudySessionList.jsx
@@ -0,0 +1,14 @@
+import StudySessionRow from "./StudySessionRow";
+
+function StudySessionList() {
+  return (
+    <section>
+      <h2>Study Sessions</h2>
+      <div>
+        <StudySessionRow />
+      </div>
+    </section>
+  );
+}
+
+export default StudySessionList;
